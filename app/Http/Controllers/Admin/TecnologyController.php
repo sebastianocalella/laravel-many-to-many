da457ba812d0cd4101ddcd6af3d92db8559b42be
Sebastiano Calella <sebastianocalella97@gmail.com>
2023-03-01 17:56:08 +0100

    TecnologyController CRU prototype

--- app/Http/Controllers/Admin/TecnologyController.php
+++ app/Http/Controllers/Admin/TecnologyController.php
@@ -3,10 +3,19 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Validation\Rule;
+use App\Models\Tecnology;
 use Illuminate\Http\Request;
 
 class TecnologyController extends Controller
 {
+
+    protected $validationRules = [
+        'name' => 'required|max:18',
+        'accent_color' => 'nullable',
+        'bg_color' => 'nullable',
+    ];
+
     /**
      * Display a listing of the resource.
      *
@@ -14,7 +23,9 @@ class TecnologyController extends Controller
      */
     public function index()
     {
-        //
+        $tecnologies = Tecnology::all();
+        return view('admin.tecnologies.index', ['tecnologies' => $tecnologies]);
+
     }
 
     /**
@@ -24,7 +35,7 @@ class TecnologyController extends Controller
      */
     public function create()
     {
-        //
+        return view('admin.tecnologies.create', ['tecnology' => new Tecnology()]);
     }
 
     /**
@@ -35,7 +46,12 @@ class TecnologyController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $data = $request->validate($this->validationRules);
+        $newtecnology = new tecnology();
+        $newtecnology->fill($data);
+        $newtecnology->save();
+
+        return redirect()->route('admin.tecnologies.index')->with('message',"tecnology $newtecnology->name has benn created succesfully");
     }
 
     /**
@@ -46,30 +62,34 @@ class TecnologyController extends Controller
      */
     public function show($id)
     {
-        //
+        return view('admin.tecnologies.show', compact('tecnology'));
     }
 
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  int  $id
+     * @param  Tecnology $tecnology
      * @return \Illuminate\Http\Response
      */
-    public function edit($id)
+    public function edit(Tecnology $tecnology)
     {
-        //
+        return view('admin.tecnologies.edit',['tecnology' => $tecnology]);
     }
 
     /**
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
+     * @param  Tecnology $tecnology
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request, Tecnology $tecnology)
     {
-        //
+        $updateValidation = $this->validationRules;
+        array_push($updateValidation, Rule::unique('tecnologies')->ignore($tecnology->id));
+        $data = $request->validate($this->validationRules);
+        $tecnology->update($data);
+        return redirect()->route('admin.tecnologies.show', compact('tecnology'));
     }
 
     /**
